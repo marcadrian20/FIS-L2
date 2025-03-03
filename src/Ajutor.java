62945aa942099f401d7cd147a024f7419363bacc
Student <student@b623-13.intranet.cs.upt.ro>
2025-03-03 10:11:33 +0200

    INITIAL COMMIT

---
+++ src/Ajutor.java
@@ -0,0 +1,14 @@
+
+public class Ajutor {
+	public void afisare()
+	{
+		System.out.println("HELLO WRLD");
+	}
+	public void info()
+	{
+		System.out.println("Nume proiect: FIS-L2");
+		System.out.println("Facultate: Universitatea Politehnica Timisoara - Automatica si Calculatoare");
+		System.out.println("Nume: Litoiu Marc-Adrian");
+	}
+
+}
