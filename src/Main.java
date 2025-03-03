c2f8801e0c385f4fa784e87a4e090a3e41f88202
marcadrian20 <marcadrian20@outlook.com>
2025-03-03 10:47:53 +0200

    TEST CLASS

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,21 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		Ajutor ajutor=new Ajutor();
+		Test test=new Test();
 		while(true)
 		{
-			System.out.println("Optiuni: 1)afisare 2)info 3)exit");
+			System.out.println("Optiuni: 1)hello 2)info 3)num 4)exit");
 			String input=scanner.nextLine();
 			switch(input)
 			{
-				case "afisare":ajutor.afisare();
+				case "hello":ajutor.afisare();
 						  break;
 				case "info":ajutor.info();
 			    		break;
-				case "exit": System.exit(0);
+				case "num":System.out.println("num: "+test.getRng(5));
+							break;
+				case "exit": scanner.close();
+							 System.exit(0);
 				default: System.out.println("retry");
 			}
 		}
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+	int rng=(int)Math.random();
+	public int getRng(int x) 
+	{
+//		System.out.println(rng);
+		return rng%x;
+	}
+	
+}
