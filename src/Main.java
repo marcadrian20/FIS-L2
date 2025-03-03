9020510a3eb4e6f14153523ccc65d264f37744d4
marcadrian20 <marcadrian20@outlook.com>
2025-03-03 10:26:09 +0200

    MODIF

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
-
+import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		Scanner scanner=new Scanner(System.in);
 		Ajutor ajutor=new Ajutor();
+		while(true)
+		{
+			System.out.println("Optiuni: 1)afisare 2)info 3)exit");
+			String input=scanner.nextLine();
+			switch(input)
+			{
+				case "afisare":ajutor.afisare();
+						  break;
+				case "info":ajutor.info();
+			    		break;
+				case "exit": System.exit(0);
+				default: System.out.println("retry");
+			}
+		}
 		
-		ajutor.info();
-		ajutor.afisare();
+		
 	}
 
 }
